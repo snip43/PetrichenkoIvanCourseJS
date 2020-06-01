32045992d3d3da5ef322e862f78819ca7df45512
snip43 <rko43@mail.ru>
2020-06-01 10:30:52 +0300

    modify cardItem

--- Project_3/src/js/main.js
+++ Project_3/src/js/main.js
@@ -38,149 +38,187 @@ document.addEventListener('DOMContentLoaded', () => {
 			}
 		})
 	})
-})
 
-//---------------------------------
+	//---------------------------------
 
-// ------------- Timer -----------
+	// ------------- Timer -----------
 
-const targetDate = new Date('2020,6,5');
+	const targetDate = new Date('2020,6,5');
 
-function createDiffDate(endtime) {
+	function createDiffDate(endtime) {
 
-	const timerDate = Date.parse(endtime) - new Date();
+		const timerDate = Date.parse(endtime) - new Date();
 
-	const leftDays = Math.floor(timerDate / (1000 * 60 * 60 * 24));
-	const leftHours = Math.floor(timerDate / (1000 * 60 * 60) % 24);
-	const leftMinutes = Math.floor((timerDate / 1000 / 60) % 60);
-	const leftSeconds = Math.floor((timerDate / 1000) % 60);
+		const leftDays = Math.floor(timerDate / (1000 * 60 * 60 * 24));
+		const leftHours = Math.floor(timerDate / (1000 * 60 * 60) % 24);
+		const leftMinutes = Math.floor((timerDate / 1000 / 60) % 60);
+		const leftSeconds = Math.floor((timerDate / 1000) % 60);
 
-	return {
-		targetDate,
-		leftDays,
-		leftHours,
-		leftMinutes,
-		leftSeconds
-	}
-}
-
-function addZero(num) {
-	if (num >=0 && num < 10) {
-		num = `0${num}`;
-		return num;
-	} else {
-		return num;
-	}
-}
-
-function insertTimerBlock(selector, endtime) {
-
-	const timerBlock = document.querySelector(selector),
-
-		timerBlockDays = timerBlock.querySelector('#days'),
-		timerBlockHours = timerBlock.querySelector('#hours'),
-		timerBlockMinutes = timerBlock.querySelector('#minutes'),
-		timerBlockSeconds = timerBlock.querySelector('#seconds'),
-		timeInterval = setInterval(updateClock, 1000);
-
-	updateClock();
-
-	function updateClock() {
-		const t = createDiffDate(endtime);
-
-		timerBlockDays.innerHTML = addZero(t.leftDays);
-		timerBlockHours.innerHTML = addZero(t.leftHours);
-		timerBlockMinutes.innerHTML = addZero(t.leftMinutes);
-		timerBlockSeconds.innerHTML = addZero(t.leftSeconds);
-
-		if(t.targetDate <= 0) {
-			clearInterval(timeInterval);
+		return {
+			targetDate,
+			leftDays,
+			leftHours,
+			leftMinutes,
+			leftSeconds
 		}
 	}
 
-}
-
-insertTimerBlock('.timer', targetDate)
-
-//--------------------------------------------------------------
-
-//------------------------ Modal window -----------------------
-
-const btnConnectMe = document.querySelectorAll('[data-modal]'),
-			modalW = document.querySelector('.modal');
-
-function openModal() {
-	modalW.classList.toggle('show');
-	document.body.classList.add('overFix');
-	clearInterval(banner);
-}
-
-function closeModal() {
-	modalW.classList.toggle('show');
-	document.body.classList.remove('overFix');
-}
-
-
-btnConnectMe.forEach((item) => {
-	item.addEventListener('click', openModal)
-}) 
-
-modalW.addEventListener('click', (e) => {
-	if (e.target.classList.contains('show') || e.target.classList.contains('modal__close')) {
-		closeModal();
+	function addZero(num) {
+		if (num >= 0 && num < 10) {
+			num = `0${num}`;
+			return num;
+		} else {
+			return num;
+		}
 	}
+
+	function insertTimerBlock(selector, endtime) {
+
+		const timerBlock = document.querySelector(selector),
+
+			timerBlockDays = timerBlock.querySelector('#days'),
+			timerBlockHours = timerBlock.querySelector('#hours'),
+			timerBlockMinutes = timerBlock.querySelector('#minutes'),
+			timerBlockSeconds = timerBlock.querySelector('#seconds'),
+			timeInterval = setInterval(updateClock, 1000);
+
+		updateClock();
+
+		function updateClock() {
+			const t = createDiffDate(endtime);
+
+			timerBlockDays.innerHTML = addZero(t.leftDays);
+			timerBlockHours.innerHTML = addZero(t.leftHours);
+			timerBlockMinutes.innerHTML = addZero(t.leftMinutes);
+			timerBlockSeconds.innerHTML = addZero(t.leftSeconds);
+
+			if (t.targetDate <= 0) {
+				clearInterval(timeInterval);
+			}
+		}
+
+	}
+
+	insertTimerBlock('.timer', targetDate)
+
+	//--------------------------------------------------------------
+
+	//------------------------ Modal window -----------------------
+
+	const btnConnectMe = document.querySelectorAll('[data-modal]'),
+		modalW = document.querySelector('.modal');
+
+	function openModal() {
+		modalW.classList.toggle('show');
+		document.body.classList.add('overFix');
+		clearInterval(banner);
+	}
+
+	function closeModal() {
+		modalW.classList.toggle('show');
+		document.body.classList.remove('overFix');
+	}
+
+
+	btnConnectMe.forEach((item) => {
+		item.addEventListener('click', openModal)
+	})
+
+	modalW.addEventListener('click', (e) => {
+		if (e.target.classList.contains('show') || e.target.classList.contains('modal__close')) {
+			closeModal();
+		}
+	})
+
+	document.addEventListener('keydown', (e) => {
+		if (e.code === 'Escape') {
+			closeModal();
+		}
+	})
+
+	// const banner = setTimeout( openModal, 10000);
+
+	function showModalByScroll() {
+		if (window.pageYOffset + document.documentElement.clientHeight >= document.documentElement.scrollHeight) {
+			openModal();
+			window.removeEventListener('scroll', showModalByScroll);
+		}
+	}
+
+	window.addEventListener('scroll', showModalByScroll);
+
+	//--------------------------------------------------------------
+
+	//-----------------Card Item ----------------------------------
+
+	class CardItem {
+		constructor(caption, alt, subtitle, descr, price, courseChange, parentSelector) {
+			this.caption = caption;
+			this.alt = alt;
+			this.subtitle = subtitle;
+			this.descr = descr;
+			this.price = price;
+			this.courseChange = courseChange;
+			this.parent = document.querySelector(parentSelector)
+
+		}
+
+		changeToRub() {
+			this.price = this.price * this.courseChange;
+		}
+
+		createCard() {
+			this.changeToRub();
+			const cardDiv = document.createElement('div');
+			cardDiv.innerHTML = `
+								<div class="menu__item">
+                    <img src="${this.caption}" alt="${this.alt}">
+                    <h3 class="menu__item-subtitle">${this.subtitle}</h3>
+                    <div class="menu__item-descr">${this.descr}</div>
+                    <div class="menu__item-divider"></div>
+                    <div class="menu__item-price">
+                        <div class="menu__item-cost">Цена:</div>
+                        <div class="menu__item-total">
+                            <span>${this.price}</span> руб/день
+                        </div>
+                    </div>
+                </div>
+		`
+			this.parent.append(cardDiv);
+		}
+	}
+
+	const cardItem_1 = new CardItem(
+		'img/tabs/vegy.jpg',
+		'vegy',
+		'Меню "Фитнес"',
+		'Меню "Фитнес" - это новый подход к приготовлению блюд: больше свежих овощей и фруктов. Продукт активных и здоровых людей. Это абсолютно новый продукт с оптимальной ценой и высоким качеством!',
+		15,
+		70,
+		'.menu .container');
+	cardItem_1.createCard();
+
+	const cardItem_2 = new CardItem(
+		'img/tabs/elite.jpg',
+		'elite',
+		'Меню “Премиум”',
+		'В меню “Премиум” мы используем не только красивый дизайн упаковки, но и качественное исполнение блюд. Красная рыба, морепродукты, фрукты - ресторанное меню без похода в ресторан!',
+		20,
+		70,
+		'.menu .container');
+	cardItem_2.createCard();
+
+	const cardItem_3 = new CardItem(
+		'img/tabs/post.jpg',
+		'post',
+		'Меню "Постное"',
+		'Меню “Постное” - это тщательный подбор ингредиентов: полное отсутствие продуктов животного происхождения, молоко из миндаля, овса, кокоса или гречки, правильное количество белков за счет тофу и импортных вегетарианских стейков.',
+		10,
+		70,
+		'.menu .container');
+
+	cardItem_3.createCard();
+
+
 })
-
-document.addEventListener('keydown', (e) => {
-	if (e.code === 'Escape') {
-		closeModal();
-	}
-})
-
-const banner = setTimeout( openModal, 10000);
-
-function showModalByScroll() {
-	if(window.pageYOffset + document.documentElement.clientHeight >= document.documentElement.scrollHeight ) {
-		openModal();
-		window.removeEventListener('scroll', showModalByScroll);
-	}
-}
-
-window.addEventListener('scroll', showModalByScroll);
-
-//--------------------------------------------------------------
-
-//-----------------Card Item ----------------------------------
-
-class CardItem {
-	constructor(caption, subtitle, descr, price) {
-		this.caption = caption;
-		this.subtitle = subtitle;
-		this.descr = descr;
-		this.price = price;
-	}
-
-	createCard() {
-		const card = document.querySelector('[data-card_1]'),
-					cardCaption = card.querySelector('img'),
-					cardSubtitle = card.querySelector('.menu__item-subtitle'),
-					cardDescr = card.querySelector('.menu__item-descr'),
-					cardPrice = card.querySelector('.menu__item-total span');
-
-		cardCaption.src = this.caption;
-		cardSubtitle.innerHTML = this.subtitle;
-		cardDescr.innerHTML = this.descr;
-		cardPrice.innerHTML = this.price;
-	}
-}
-
-
-
-
-const cardItem_1 = new CardItem(
-	'img/tabs/vegy.jpg',
-	'Меню "Фитнес"',
-	'Меню "Фитнес" - это новый подход к приготовлению блюд: больше свежих овощей и фруктов. Продукт активных и здоровых людей. Это абсолютно новый продукт с оптимальной ценой и высоким качеством!', 229);
-cardItem_1.createCard();
-
 
