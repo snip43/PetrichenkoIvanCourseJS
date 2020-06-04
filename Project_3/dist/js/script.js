64c5aa273bd2b832624938c3e483d43dc234ab4f
snip43 <rko43@mail.ru>
2020-06-04 17:27:33 +0300

    create json

--- Project_3/dist/js/script.js
+++ Project_3/dist/js/script.js
@@ -271,7 +271,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
   const forms = document.querySelectorAll('form');
   const message = {
-    loading: 'Загрузка...',
+    loading: 'img/form/spinner.svg',
     success: 'Спасибо! Скоро с Вами свяжемся',
     failure: 'Что-то пошло не так...'
   };
@@ -282,29 +282,56 @@ document.addEventListener('DOMContentLoaded', () => {
   function postData(form) {
     form.addEventListener('submit', e => {
       e.preventDefault();
-      const statusMessage = document.createElement('div');
-      statusMessage.classList.add('status');
-      statusMessage.textContent = message.loading;
-      form.append(statusMessage);
+      const statusMessage = document.createElement('img');
+      statusMessage.src = message.loading;
+      statusMessage.style.cssText = `
+			display: block;
+			margin: 0 auto;
+		`;
+      form.insertAdjacentElement('afterend', statusMessage);
       const request = new XMLHttpRequest();
-      request.open('POST', 'server.php'); // request.setRequestHeader('Content-type','multipart/form-data')
-
+      request.open('POST', 'server.php');
+      request.setRequestHeader('Content-type', 'application/json');
       const formData = new FormData(form);
-      request.send(formData); //затем отслеживаем "Load" конечную загрузку нашего запроса
+      const object = {};
+      formData.forEach(function (value, key) {
+        object[key] = value;
+      });
+      const json = JSON.stringify(object);
+      request.send(json); //затем отслеживаем "Load" конечную загрузку нашего запроса
 
       request.addEventListener('load', () => {
         if (request.status === 200) {
           console.log(request.response);
-          statusMessage.textContent = message.success;
+          showThanksModal(message.success);
           form.reset();
-          setTimeout(() => {
-            statusMessage.remove();
-          }, 2000);
+          statusMessage.remove();
         } else {
-          statusMessage.textContent = message.failure;
+          showThanksModal(message.failure);
         }
       });
     });
+  }
+
+  function showThanksModal() {
+    const prevModalDialog = document.querySelector('.modal__dialog');
+    prevModalDialog.classList.add('hide');
+    openModal();
+    const thanksModal = document.createElement('div');
+    thanksModal.classList.add('modal__dialog');
+    thanksModal.innerHTML = `
+		<div class="modal__content">
+			<div class="modal__title">${message}</div>
+			<div class="modal__close" data-close>&times;</div>
+		</div>
+	`;
+    document.querySelector('.modal').append(thanksModal);
+    setTimeout(() => {
+      thanksModal.remove();
+      prevModalDialog.classList.add('show');
+      prevModalDialog.classList.remove('hide');
+      closeModal();
+    }, 4000);
   }
 });
 
